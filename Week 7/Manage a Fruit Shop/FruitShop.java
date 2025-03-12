0dcf934c50ec31ea2f6f14c29878edffd3c2eca3
phWiz-2412 <161670466+phWiz-2412@users.noreply.github.com>
2025-03-12 19:41:59 +0700

    FruitShop.java

---
+++ Week 7/Manage a Fruit Shop/FruitShop.java
@@ -0,0 +1,23 @@
+public class FruitShop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        FruitController controller = new FruitController();
+        while (true) {
+            System.out.println("FRUIT SHOP SYSTEM");
+            System.out.println("1. Create Fruit");
+            System.out.println("2. View orders");
+            System.out.println("3. Shopping");
+            System.out.println("4. Exit");
+            System.out.print("Your choice: ");
+            int choice = sc.nextInt();
+            sc.nextLine();
+            switch (choice) {
+                case 1 -> controller.createFruit();
+                case 2 -> controller.viewOrders();
+                case 3 -> controller.shopping();
+                case 4 -> { return; }
+                default -> System.out.println("Invalid choice!");
+            }
+        }
+    }
+}
