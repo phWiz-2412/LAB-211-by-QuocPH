c449bd65585f1750b4db69d6591dd5472841e4e8
phWiz-2412 <161670466+phWiz-2412@users.noreply.github.com>
2025-03-12 19:41:09 +0700

    Fruit.java

---
+++ Week 7/Manage a Fruit Shop/Fruit.java
@@ -0,0 +1,15 @@
+class Fruit {
+    int id;
+    String name;
+    String origin;
+    double price;
+    int quantity;
+
+    public Fruit(int id, String name, String origin, double price, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.origin = origin;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
