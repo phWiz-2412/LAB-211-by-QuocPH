544e58670243a80a0824b0e98b4462d3f4e615f2
phWiz-2412 <161670466+phWiz-2412@users.noreply.github.com>
2025-03-18 08:43:13 +0700

    Update ShapeController.java

--- Week 8/Shapes/ShapeController.java
+++ Week 8/Shapes/ShapeController.java
@@ -1,9 +1,14 @@
 class ShapeController {
-    public void displayShapes() {
-        Shape[] shapes = {
+    private final Shape[] shapes;
+    
+    public ShapeController() {
+        shapes = new Shape[]{
             new Circle(5), new Square(4), new Triangle(3, 6),
             new Sphere(3), new Cube(2), new Tetrahedron(4)
         };
+    }
+    
+    public void displayShapes() {
         for (Shape shape : shapes) {
             shape.display();
         }
